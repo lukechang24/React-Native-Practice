d069484410f4307a9b48b08ab7d60e2131e9db8d
lukechang24 <lukechang244@gmail.com>
2021-11-29 17:19:13 -0800

    initialized expo project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, SafeAreaView, TouchableHighlight, Button } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" onPress={() => console.log("Tapped")}/>
+    </SafeAreaView>
   );
 }
 
